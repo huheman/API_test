a7e6aa816475dc85112f9896e23a4e18965f155f
hepeng <876651731@qq.com>
2019-03-20 19:59:23 +0800

    first commit

---
+++ API_test/src/binarystring/ByteString.java
@@ -0,0 +1,15 @@
+package binarystring;
+
+public class ByteString {
+    public static void main(String[] args) {
+        byte[] bytes = "小鹏头".getBytes();
+        int count = 1;
+        for (byte aByte : bytes) {
+            //System.out.print(":"+Integer.toHexString(aByte)+" ");
+            System.out.printf(aByte+":%x   ",aByte);
+            if (count++%3==0) {
+                System.out.println();
+            }
+        }
+    }
+}
