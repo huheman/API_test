d2aae983dce7c3d3327fb7a329002f554e7f9a90
hepeng <876651731@qq.com>
2019-03-21 10:51:40 +0800

    加上fileWalker案例

---
+++ file_sample/src/filetest/FileWalker.java
@@ -0,0 +1,51 @@
+package filetest;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+/**
+ * 获得指定文件夹下所有的java文件(不考虑子文件夹的)并输出到控制台
+ */
+public class FileWalker {
+    public static void main(String[] args) {
+        Set<File> walk = walk("..",".*\\.java");
+        for (File file : Objects.requireNonNull(walk)) {
+            System.out.println(file);
+        }
+    }
+
+    public static Set<File> walk(File file, Predicate<File> predicate) {
+        if (!file.exists()) return null;
+        HashSet<File> fileHashSet = new HashSet<>();
+        if (file.isFile()) {
+            if (predicate.test(file)) fileHashSet.add(file);
+        } else {
+            File[] files = file.listFiles(f -> f.isDirectory() || predicate.test(f));
+            for (File fileListed : Objects.requireNonNull(files)) {
+                fileHashSet.addAll(Objects.requireNonNull(walk(fileListed, predicate)));
+            }
+        }
+        return fileHashSet;
+    }
+
+    public static Set<File> walk(File file) {
+        return walk(file, ".*");
+    }
+
+    public static Set<File> walk(File file, String pattern) {
+        return walk(file, f -> Pattern.matches(pattern, f.getName()));
+    }
+
+    public static Set<File> walk(String pathName) {
+        return walk(new File(pathName));
+    }
+
+    public static Set<File> walk(String pathName, String pattern) {
+        return walk(new File(pathName), pattern);
+    }
+}
+
